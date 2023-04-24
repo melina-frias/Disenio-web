f10396451870684fd84f6a6c6d069cc4313e8e3e
melina frias <meel.frias@gmail.com>
2023-04-24 20:43:23 -0300

    arreglo responsive

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -7,69 +7,72 @@ import { motion } from 'framer-motion'
 import React from 'react';
 
 const Formulario = () => {
-    const  InfoContacto =[
-        {icono: phone,
-        title: 'Ponerse en contacto',
-        subtitle: '(96) 6587265',
-        id: 1,
+    const InfoContacto = [
+        {
+            icono: phone,
+            title: 'Ponerse en contacto',
+            subtitle: '(96) 6587265',
+            id: 1,
         },
-        {icono: email,
-        title: 'Email',
-        subtitle: 'Hola@studio.co',
-        id: 2,
+        {
+            icono: email,
+            title: 'Email',
+            subtitle: 'Hola@studio.co',
+            id: 2,
         },
-        {icono: pin,
-        title: 'Mapa de calle',
-        subtitle: 'Khulna bangledesh.9000',
-        id: 3,
+        {
+            icono: pin,
+            title: 'Mapa de calle',
+            subtitle: 'Khulna bangledesh.9000',
+            id: 3,
         },
     ];
-    return(
+    return (
         <motion.div id='Formulario'
-        className='container 
+            className='container 
         seccionForm'
-        initial= {{opacity:0}}
-        transition={{delay:1,duration:1}}
-        animate={{opacity:1}}
-        final={{opacity:0}}
+            initial={{ opacity: 0 }}
+            transition={{ delay: 1, duration: 1 }}
+            animate={{ opacity: 1 }}
+            final={{ opacity: 0 }}
         >
             <div className='columns contenedorForm'>
                 <div className="column">
                     <h2 className='tituloForm'>Ponerse en contacto</h2>
 
-                {InfoContacto.map(dato => (
-                    <div  key={dato.id}
-                    className='contenedorInfo is-flex'>
-                        <img src={dato.icono} alt={dato.title}/>
-                        <div className='datos'>
-                            <h3>{dato.title}</h3>
-                            <p>{dato.subtitle}</p>
+                    {InfoContacto.map(dato => (
+                        <div key={dato.id}
+                            className='contenedorInfo is-flex'>
+                            <img src={dato.icono} alt={dato.title} />
+                            <div className='datos'>
+                                <h3>{dato.title}</h3>
+                                <p>{dato.subtitle}</p>
+                            </div>
                         </div>
-                    </div>
                     ))}
-                    
+
                 </div>
                 <div className="column
                 formulario">
-                <form action="https://formspree.io/f/xdovgazz"
-                method="POST">
-                    <h2 className='tituloForm'>Hable con nosotros</h2>
+                    <form action="https://formspree.io/f/xdovgazz"
+                        method="POST">
+                        <h2 className='tituloForm'>Hable con nosotros</h2>
                         <label>Nombre completo</label>
                         <input type="text" name='nombre'
-                        placeholder='Ingrese su nombre'/>
+                            placeholder='Ingrese su nombre' />
 
                         <label>Su compañia</label>
                         <input type="text" name='compañia'
-                        placeholder='Ingrese el nombre de su compañia'/>
+                            placeholder='Ingrese el nombre de su compañia' />
 
                         <label>Su dirección de correo electrónico</label>
-                        <input type="email" name="email" id="correo" 
-                        placeholder='Ingrese su dirección de correo electrónico ' />
+                        <input type="email" name="email" id="correo"
+                            placeholder='Ingrese su dirección de correo electrónico ' />
 
                         <label>Mensaje</label>
                         <textarea name="message" id="mensaje" className='textarea'
-                        placeholder='Escriba su mensaje'></textarea>
-                        
+                            placeholder='Escriba su mensaje'></textarea>
+
                         <button type="submit" className='btnGral'>Mensaje enviado</button>
                     </form>
                 </div>
